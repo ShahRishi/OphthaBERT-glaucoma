4f0be9b1f3be52e57ca0d0566094d48f583f2f13
Rishi Shah <shahrishi108@gmail.com>
2025-01-20 12:26:26 -0800

    consolidated adapters to hf hub

--- api.py
+++ api.py
@@ -1,18 +1,26 @@
 from flask import Flask, request, jsonify
 from transformers import DistilBertForSequenceClassification, DistilBertTokenizer
 import torch
-from adapters import AutoAdapterModel
 
+# Initialize the Flask app
 app = Flask(__name__)
 
-model = DistilBertForSequenceClassification.from_pretrained("ShahRishi/OphthoBERT")
-tokenizer = DistilBertTokenizer.from_pretrained("ShahRishi/OphthoBERT")
-model.load_adapter("./models/ophthabert-binary")
+# Load the tokenizer and models
+tokenizer = DistilBertTokenizer.from_pretrained("ShahRishi/OphthaBERT")
 
-model.eval()
+# Binary classification model
+binary_model = DistilBertForSequenceClassification.from_pretrained("ShahRishi/OphthoBERT", num_labels=2)
+binary_model.load_adapter("ShahRishi/ophthabert-glaucoma-binary")
+binary_model.eval()
 
-@app.route('/predict', methods=['POST'])
-def predict():
+# Subtypes classification model
+subtypes_model = DistilBertForSequenceClassification.from_pretrained("ShahRishi/OphthoBERT", num_labels=5)
+subtypes_model.load_adapter("ShahRishi/ophthabert-glaucoma-subtypes")
+subtypes_model.eval()
+
+
+@app.route('/predict/binary', methods=['POST'])
+def predict_binary():
     try:
         data = request.json
         if 'text' not in data:
@@ -22,11 +30,11 @@ def predict():
         inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=512)
 
         with torch.no_grad():
-            outputs = model(**inputs)
+            outputs = binary_model(**inputs)
         logits = outputs.logits
         predicted_class = torch.argmax(logits, dim=1).item()
 
-        label_map = {0: "control", 1: "case"} 
+        label_map = {0: "control", 1: "case"}
         result = {
             'text': text,
             'predicted_label': label_map[predicted_class]
@@ -36,5 +44,35 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+
+
+@app.route('/predict/subtypes', methods=['POST'])
+def predict_subtypes():
+    try:
+        data = request.json
+        if 'text' not in data:
+            return jsonify({'error': 'Text field is required'}), 400
+
+        text = data['text']
+        inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=512)
+
+        with torch.no_grad():
+            outputs = subtypes_model(**inputs)
+        logits = outputs.logits
+        predicted_class = torch.argmax(logits, dim=1).item()
+
+        # Map prediction to label
+        label_map = {0: "POAG", 1: "Other", 2: "PDS", 3:"PACG", 4:"PXF"}
+        result = {
+            'text': text,
+            'predicted_label': label_map[predicted_class]
+        }
+        return jsonify(result)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
 if __name__ == '__main__':
+    # Run the Flask app
     app.run(host='0.0.0.0', port=8080)
